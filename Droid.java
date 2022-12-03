9aa681dc705c7a376aa78d1d910027986ad0fd39
md-cyber <>
2022-12-03 15:10:22 +0530

    complet charging,transfer charging methods

--- Droid.java
+++ Droid.java
@@ -1,12 +1,60 @@
 public class Droid{
-    
+    //instance fields
+    String name;
+    int batteryLevel = 100;
+
     //constructor
-    public Droid(){
+    public Droid(String droidName){
+        name = droidName;
         
     }
 
+    //methods
+    public void performTask(String task){
+        System.out.println(name + " is performing "+task);
+        batteryLevel -= 10;
+    }
+    //Display Battery percentage
+    public void displayBatteryLevel(){
+        System.out.println(name + " has: "+batteryLevel + "% battery left");
+    }
+    //Charge droid
+    public void chargDroid(int percentage){
+        batteryLevel += percentage;
+    }
+
+    //Transfer Battery with other droid
+    public void transferEnergy(Droid receverDroidName, int percenteage){
+        receverDroidName.chargDroid(percenteage);
+        batteryLevel -= percenteage;
+        System.out.println("Sucessfully charged "+ receverDroidName.name + " " + percenteage);
+    }
     //main 
     public static void main(String[] args){
 
+        //instantiating Droid cody
+        Droid cody = new Droid("Cody");
+        System.out.println(cody);
+
+        //making cody perform learn to fly
+        cody.performTask("flying");
+        cody.displayBatteryLevel();
+
+        //makig droid jimmy
+        Droid jimmy = new Droid("jimmy");
+        jimmy.displayBatteryLevel();
+        jimmy.performTask("cleaning");
+        jimmy.performTask("grass cutting");
+        jimmy.performTask("scanning floor");
+        jimmy.displayBatteryLevel();
+
+        cody.transferEnergy(jimmy, 20);
+        jimmy.displayBatteryLevel();
+        cody.displayBatteryLevel();
+    }
+
+    public String toString(){
+        String description = "Hello I am "+name + " Droid";
+        return description;
     }
 }
